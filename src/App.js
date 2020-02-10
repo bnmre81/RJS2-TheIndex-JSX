3a3acfdd29ba82b7fc5706cf1ddde3c211f9a656
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-02-10 17:04:40 +0300

    task 2 reac

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App() {
                   <div className="image">
                     <img
                       className="card-img-top img-fluid"
-                      src="https://nation.com.pk/digital_images/large/2015-05-10/jk-rowling-believes-in-standing-up-to-bullies-1431261222-3596.jpeg"
-                      alt="J.K. Rowling"
+                      src={authors[0].imageUrl}
+                      alt={`${authors[0].first_name} ${authors[0].last_name}`}
                     />
                   </div>
                   <div className="card-body">
@@ -45,8 +45,8 @@ function App() {
                   <div className="image">
                     <img
                       className="card-img-top img-fluid"
-                      src="https://typeset-beta.imgix.net/elite-daily/2016/03/06144207/elite-daily-George-RR-Martin.jpg?w=800&h=800&auto=format&q=70&fit=crop&crop=faces"
-                      alt="{authors[1].first_name} {authors[1].last_name}"
+                      src={authors[1].imageUrl}
+                      alt={`${authors[1].first_name} ${authors[1].last_name}`}
                     />
                   </div>
                   <div className="card-body">
@@ -66,8 +66,8 @@ function App() {
                   <div className="image">
                     <img
                       className="card-img-top img-fluid"
-                      src="https://images.8tracks.com/cover/i/010/010/920/tolkeinbigger-2362.jpg?rect=166,0,500,500&q=98&fm=jpg&fit=max"
-                      alt="{authors[2].first_name} {authors[2].last_name}"
+                      src={authors[2].imageUrl}
+                      alt={`${authors[2].first_name} ${authors[2].last_name}`}
                     />
                   </div>
                   <div className="card-body">
@@ -88,8 +88,8 @@ function App() {
                   <div className="image">
                     <img
                       className="card-img-top img-fluid"
-                      src="https://literature.britishcouncil.org/assets/Uploads/writers/_resampled/CroppedImage680680-Margaret-Atwood-c-Jean-Malek-web.jpg"
-                      alt="{authors[3].first_name} {authors[3].last_name}"
+                      src={authors[3].imageUrl}
+                      alt={`${authors[3].first_name} ${authors[3].last_name}`}
                     />
                   </div>
                   <div className="card-body">
